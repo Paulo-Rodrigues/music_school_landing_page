57130ff07b730b3abe31662e59274eaac2d7a2b3
Paulo Rodrigues <paulo.rodrigues@rebase.com.br>
2021-05-04 00:11:52 -0300

    add menu component(init)

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Menu from '../../components/Menu';
+import Banner from '../../components/Banner';
 
 function Homepage() {
-  return <h1>Homepage</h1>;
+  return (
+    <>
+      <Menu />
+      <Banner />
+    </>
+  );
 }
 
 export default Homepage;
